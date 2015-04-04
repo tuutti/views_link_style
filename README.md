486ee430b2a594e2074f15fdedab96b55d9e525a
Lari Rauno <tuutti@iki.fi>
2015-04-05 00:59:52 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+### Usage
+
+- Add some field that outputs absolute / relative url. For example "Content: Path", which is an URL alias to the node content.
+- Change format to "Link" and fill "Link token" field. You may use field tokens from as per the "Replacement patterns" used in "Rewrite the output of this field" for all fields.
---
+++ src/Plugin/views/style/HtmlLink.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * @file
+ * Definition of Drupal\views_link_style\Plugin\views\style\Link.
+ */
+
+namespace Drupal\views_link_style\Plugin\views\style;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\views\Plugin\views\style\StylePluginBase;
+
+/**
+ * Style plugin to render each item in a link.
+ *
+ * @ingroup views_style_plugins
+ *
+ * @ViewsStyle(
+ *   id = "html_views_link",
+ *   title = @Translation("Link"),
+ *   help = @Translation("Displays rows as a link."),
+ *   theme = "views_link_style_link",
+ *   display_types = {"normal"}
+ * )
+ */
+class HtmlLink extends StylePluginBase {
+
+  /**
+   * Does the style plugin allows to use style plugins.
+   *
+   * @var bool
+   */
+  protected $usesRowPlugin = TRUE;
+
+  /**
+   * Does the style plugin support custom css class for the rows.
+   *
+   * @var bool
+   */
+  protected $usesRowClass = TRUE;
+
+  /**
+   * Set default options.
+   */
+  protected function defineOptions() {
+    $options = parent::defineOptions();
+
+    $options['class'] = array('default' => '');
+    $options['link_token'] = array('default' => '');
+
+    return $options;
+  }
+
+  /**
+   * Render the given style.
+   */
+  public function buildOptionsForm(&$form, FormStateInterface $form_state) {
+    parent::buildOptionsForm($form, $form_state);
+
+    $form['link_token'] = array(
+      '#required' => TRUE,
+      '#type' => 'textfield',
+      '#title' => $this->t('Link token'),
+      '#default_value' => $this->options['link_token'],
+    );
+  }
+
+}
